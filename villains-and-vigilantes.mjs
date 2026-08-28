ccfd2097fa73905353df33cfdc1d31a4895d50eb
Pogybait <richbyers@gmail.com>
2026-08-28 13:25:09 -0500

    Register character data model

--- villains-and-vigilantes.mjs
+++ villains-and-vigilantes.mjs
@@ -1,2 +1,12 @@
-// Villains & Vigilantes Foundry VTT System
-// System ID: villains-and-vigilantes
+import { CharacterData } from "./module/data-models/character.mjs";
+
+/**
+ * Villains & Vigilantes Foundry VTT System
+ * System ID: villains-and-vigilantes
+ */
+
+Hooks.once("init", () => {
+  console.log("Villains and Vigilantes | Initializing system");
+
+  CONFIG.Actor.dataModels.character = CharacterData;
+});
